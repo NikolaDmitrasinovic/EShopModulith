c3dcc695b764cebfbebe4291ee60bca6af990461
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-12-08 20:19:47 +0100

    feat: add LendingModule

---
+++ src/Modules/Lending/Lending/LendingModule.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lending;
+public static class LendingModule
+{
+    public static IServiceCollection AddCatalogModule(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // Add services to the container.
+        //services
+        //    .AddApplicationServices()
+        //    .AddInfrastructureServices(configuration)
+        //    .AddApiServices(configuration);
+
+        return services;
+    }
+}
