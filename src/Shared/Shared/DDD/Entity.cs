b813cb61d2851c568706cc3cdc4ba58a4cfe2d14
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-12-11 21:08:05 +0100

    chore: fix formatting issues

--- src/Shared/Shared/DDD/Entity.cs
+++ src/Shared/Shared/DDD/Entity.cs
@@ -1,8 +1,9 @@
 ﻿
 namespace Shared.DDD;
+
 public abstract class Entity<T> : IEntity<T>
 {
-    public required T Id {  get; set; }
+    public required T Id { get; set; }
     public DateTime? CreatedAt { get; set; }
     public string? CreatedBy { get; set; }
     public DateTime? LastModified { get; set; }
