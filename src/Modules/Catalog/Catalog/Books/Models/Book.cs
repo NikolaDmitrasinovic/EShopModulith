fbdd713680dcf41fce933f65260ccc149fe954d0
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-12-14 10:07:44 +0100

    feat: add new entity, Book

---
+++ src/Modules/Catalog/Catalog/Books/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿using Shared.DDD;
+
+namespace Catalog.Books.Models;
+public class Book : Entity<Guid>
+{
+    public string Title { get; set; } = default!;
+    public List<string> Category { get; set; } = [];
+    public string Description { get; set; } = default!;
+    public string ImageFile { get; set; } = default!;
+    public decimal Score { get; set; }
+}
