edde439f9be755855e6d14687b06a5c06dd80bf9
Nikola Dmitrasinovic <118458592+NikolaDmitrasinovic@users.noreply.github.com>
2025-12-18 18:55:11 +0100

    Develop Catalog Module (#12)
    # Description
    Product is added as an entity.
    Two domain events are included ( price changed and product created )

---
+++ src/Modules/Catalog/Catalog/Products/Models/Product.cs
@@ -0,0 +1,47 @@
+﻿using Catalog.Products.Events;
+
+namespace Catalog.Books.Models;
+public class Product : Aggregate<Guid>
+{
+    public string Name { get; private set; } = default!;
+    public List<string> Category { get; private set; } = [];
+    public string Description { get; private set; } = default!;
+    public string ImageFile { get; private set; } = default!;
+    public decimal Price { get; private set; }
+
+    public static Product Create(Guid id, string title, List<string> category, string description, string imageFile, decimal score)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(title);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(score);
+
+        var product = new Product
+        {
+            Id = id,
+            Name = title,
+            Category = category,
+            ImageFile = imageFile,
+            Price = score
+        };
+
+        product.AddDomainEvent(new ProductCreatedEvent(product));
+
+        return product;
+    }
+    
+    public void Update(string title, List<string> category, string description, string imageFile, decimal price)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(title);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        Name = title;
+        Category = category;
+        Description = description;
+        ImageFile = imageFile;
+
+        if (Price != price)
+        {
+            Price = price;
+            AddDomainEvent(new ProductPriceChangedEvent(this));
+        }
+    }
+}
