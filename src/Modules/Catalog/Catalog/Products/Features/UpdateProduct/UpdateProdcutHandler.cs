65eecb10c4238fe067a5dd800aa92418256ec6ca
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-01-20 19:35:49 +0100

    116 develp update product command

---
+++ src/Modules/Catalog/Catalog/Products/Features/UpdateProduct/UpdateProdcutHandler.cs
@@ -0,0 +1,44 @@
+﻿
+
+namespace Catalog.Products.Features.UpdateProduct;
+
+public record UpdateProductCommand(ProductDto Product)
+    : ICommand<UpdateProductResult>;
+
+public record UpdateProductResult(bool IsSuccess);
+
+internal class UpdateProdcutHandler(CatalogDbContext dbContext) 
+    : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+{
+    public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    {
+        // Update
+        // save to db
+        // return result
+
+        var product = await dbContext.Products
+            .FindAsync([command.Product.Id], cancellationToken);
+
+        if (product == null)
+        {
+            throw new Exception($"Product not found: {command.Product.Id}");
+        }
+
+        UpdateProdcutWihtNewVaues(product, command.Product);
+
+        dbContext.Products.Update(product);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new UpdateProductResult(true);
+    }
+
+    private void UpdateProdcutWihtNewVaues(Product product, ProductDto productDto)
+    {
+        product.Update(
+            productDto.Name,
+            productDto.Category,
+            productDto.Description,
+            productDto.ImageFile,
+            productDto.Price);
+    }
+}
