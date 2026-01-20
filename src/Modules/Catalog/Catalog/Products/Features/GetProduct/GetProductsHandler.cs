0c0be784bd1218aa2c907dc29a3b38b80e7bf0ff
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-01-20 20:17:20 +0100

    118 product query

---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProduct/GetProductsHandler.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace Catalog.Products.Features.GetProduct;
+
+public record GetPRoductQuery()
+    : IQuery<GetProductResult>;
+
+public record GetProductResult(IEnumerable<ProductDto> Products);
+
+internal class GetProductsHandler(CatalogDbContext dbContext)
+    : IQueryHandler<GetPRoductQuery, GetProductResult>
+{
+    public async Task<GetProductResult> Handle(GetPRoductQuery query, CancellationToken cancellationToken)
+    {
+        // get products
+        // return result
+
+        var products = await dbContext.Products
+            .AsNoTracking()
+            .OrderBy(p=>p.Name)
+            .ToListAsync(cancellationToken);
+
+        var productDtos = ProjectToProductDto(products);
+
+        return new GetProductResult(productDtos);
+    }
+
+    private List<ProductDto> ProjectToProductDto(List<Product> products)
+    {
+        foreach (var product in products)
+        {
+
+        }
+        return [];
+    }
+}
