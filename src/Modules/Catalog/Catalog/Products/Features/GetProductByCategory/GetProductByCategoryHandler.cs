d87b6351b4af91d137fa534511513458ba7e7200
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-01-21 20:35:37 +0100

    121 develop get product by category

---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Catalog.Products.Features.GetProductByCategory;
+
+public record GetProductByCategoryQuery(string Catogory)
+    : IQuery<GetProductByCategoryResult>;
+
+public record GetProductByCategoryResult(IEnumerable<ProductDto> Products);
+
+internal class GetProductByCategoryHandler(CatalogDbContext dbContext)
+    : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+{
+    public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        var products = await dbContext.Products
+            .AsNoTracking()
+            .Where(p=>p.Category.Contains(query.Catogory))
+            .OrderBy(p=>p.Name)
+            .ToListAsync(cancellationToken);
+
+        // mapping product to productdto
+        var productDtos = products.Adapt<List<ProductDto>>();
+
+        return new GetProductByCategoryResult(productDtos);
+    }
+}
