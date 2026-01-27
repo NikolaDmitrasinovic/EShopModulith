a56de978b74f8ce9e67dd38274c829305325cf46
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-01-27 20:21:54 +0100

    129 create endpoint

---
+++ src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using Carter;
+using Microsoft.AspNetCore.Routing;
+
+namespace Catalog.Products.Features.CreateProduct;
+
+public record CreateProductRequest(ProductDto Product);
+public record CreateProductResponse(Guid Id);
+
+public class CreateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        throw new NotImplementedException();
+    }
+}
