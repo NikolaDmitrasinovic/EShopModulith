0e480ba645822253854f8ea08691e6ce0fe574b6
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-02-17 14:35:49 +0100

    151 global exception handling

--- src/Bootstraper/Api/Program.cs
+++ src/Bootstraper/Api/Program.cs
@@ -1,13 +1,9 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//builder.Services.AddCarter(configurator: config =>
-//{
-//    var catalogModules = typeof(CatalogModule).Assembly.GetTypes()
-//    .Where(t => t.IsAssignableTo(typeof(CatalogModule))).ToArray();
-
-//    config.WithModules(catalogModules);
-//});
 builder.Services
     .AddCarterWithAsseblies(typeof(CatalogModule).Assembly);
 
@@ -25,5 +21,29 @@ app
     .UseCatalogModule()
     .UseBasketModule()
     .UseLendingModule();
+
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if (exception == null) return;
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.StackTrace
+        };
+
+        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+        logger.LogError(exception, exception.Message);
+
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/problem+json";
+
+        await context.Response.WriteAsJsonAsync(problemDetails);
+    });
+});
 
 app.Run();
