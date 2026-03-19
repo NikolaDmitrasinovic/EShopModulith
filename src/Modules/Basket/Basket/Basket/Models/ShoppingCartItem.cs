dcf1e908b1986b695511609ae03f4297e01d65aa
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-03-19 21:10:31 +0100

    164 shoping cart domain entity models

---
+++ src/Modules/Basket/Basket/Basket/Models/ShoppingCartItem.cs
@@ -0,0 +1,24 @@
+﻿using Shared.DDD;
+
+namespace Basket.Basket.Models;
+
+public class ShoppingCartItem : Entity<Guid>
+{
+    public Guid ShoppingCardId { get; private set; } = default!;
+    public Guid ProductId { get; private set; } = default!;
+    public int Quantity { get; internal set; } = default!;
+    public string Color { get; private set; } = default!;
+
+    public decimal Price { get; private set; } = default;
+    public string ProductName { get; private set; } = default!;
+
+    public ShoppingCartItem(Guid shoppingCardId, Guid productId, int quantity, string color, decimal price, string productName)
+    {
+        ShoppingCardId = shoppingCardId;
+        ProductId = productId;
+        Quantity = quantity;
+        Color = color;
+        Price = price;
+        ProductName = productName;
+    }
+}
