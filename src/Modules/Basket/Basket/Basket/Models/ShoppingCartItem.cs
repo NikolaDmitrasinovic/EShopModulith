049fb74dfa7eb2d1404b4df97d7844e49c72c87a
Nikola Dmitrasinovic <118458592+NikolaDmitrasinovic@users.noreply.github.com>
2026-03-20 20:32:11 +0100

    Section 16 - Develop Basket Module with Vertical Slice (#19)
    * 164 shoping cart domain entity models
    
    * 165 develop ddd rich-domain model intity

---
+++ src/Modules/Basket/Basket/Basket/Models/ShoppingCartItem.cs
@@ -0,0 +1,26 @@
+﻿using Shared.DDD;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Basket.Basket.Models;
+
+public class ShoppingCartItem : Entity<Guid>
+{
+    public Guid ShoppingCardId { get; private set; } = default!;
+    public Guid ProductId { get; private set; } = default!;
+    public int Quantity { get; internal set; } = default!;
+    public string Color { get; private set; } = default!;
+
+    public decimal Price { get; private set; } = default;
+    public string ProductName { get; private set; } = default!;
+
+    [SetsRequiredMembers]
+    internal ShoppingCartItem(Guid shoppingCardId, Guid productId, int quantity, string color, decimal price, string productName)
+    {
+        ShoppingCardId = shoppingCardId;
+        ProductId = productId;
+        Quantity = quantity;
+        Color = color;
+        Price = price;
+        ProductName = productName;
+    }
+}
