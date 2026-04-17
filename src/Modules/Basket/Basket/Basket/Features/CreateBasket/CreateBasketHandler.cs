c8cff8df1231044499670334ecd80b6255f196df
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-04-17 18:47:35 +0200

    182 create basket handler

---
+++ src/Modules/Basket/Basket/Basket/Features/CreateBasket/CreateBasketHandler.cs
@@ -0,0 +1,26 @@
+﻿using Basket.Basket.Dtos;
+using FluentValidation;
+using Shared.CQRS;
+
+namespace Basket.Basket.Features.CreateBasket;
+
+public record CreateBasketCommand(ShoppingCartDto ShoppingCart) : ICommand<CreateBasketResult>;
+
+public record CreateBasketResult(Guid Id);
+
+public class CreateBasketCommandValidator : AbstractValidator<CreateBasketCommand>
+{
+    public CreateBasketCommandValidator()
+    {
+        RuleFor(x=>x.ShoppingCart.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+
+internal class CreateBasketHandler
+        : ICommandHandler<CreateBasketCommand, CreateBasketResult>
+{
+    public Task<CreateBasketResult> Handle(CreateBasketCommand command, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
