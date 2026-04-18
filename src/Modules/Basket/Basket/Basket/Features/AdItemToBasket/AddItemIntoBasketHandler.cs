c86d86b90b30c4582c842ddbbe921782878b2d07
Nikola Dmitrasinovic <118458592+NikolaDmitrasinovic@users.noreply.github.com>
2026-04-18 19:32:55 +0200

    Section 18 - Develop Basket Application use case (#20)
    * 180 develop sqrs folder structure
    
    * 181 dvelop dtos
    
    * 182 create basket handler
    
    * 183 develop create basket hander
    
    * 184 delete basket handler
    
    * 185 basket queries
    
    * 186 add item to basket
    
    * 187 remove item from basket

---
+++ src/Modules/Basket/Basket/Basket/Features/AdItemToBasket/AddItemIntoBasketHandler.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Basket.Basket.Features.AdItemToBasket;
+
+public record AddItemIntoBasketCommand(string UserName, ShoppingCartItemDto ShoppingCartItem)
+    : ICommand<AddItemIntoBasketResult>;
+
+public record AddItemIntoBasketResult(Guid Id);
+
+public class AddItemIntoBasketCommandValidator : AbstractValidator<AddItemIntoBasketCommand>
+{
+    public AddItemIntoBasketCommandValidator()
+    {
+        RuleFor(x=> x.UserName)
+            .NotEmpty()
+            .WithMessage("UserName is required.");
+        RuleFor(x=> x.ShoppingCartItem.ProductId)
+            .NotEmpty()
+            .WithMessage("ProductId is required.");
+        RuleFor(x=>x.ShoppingCartItem.Quantity)
+            .GreaterThan(0)
+            .WithMessage("Quantity must be greater than 0.");
+    }
+}
+
+internal class AddItemIntoBasketHandler(BasketDbContext dbContext)
+    : ICommandHandler<AddItemIntoBasketCommand, AddItemIntoBasketResult>
+{
+    public async Task<AddItemIntoBasketResult> Handle(AddItemIntoBasketCommand request, CancellationToken cancellationToken)
+    {
+        var shoppingCart = await dbContext.ShoppingCarts
+            .Include(x => x.Items)
+            .SingleOrDefaultAsync(x => x.UserName == request.UserName);
+
+        if (shoppingCart is null)
+            throw new BasketNotFoundException(request.UserName);
+
+        shoppingCart.AddItem(request.ShoppingCartItem.ProductId, request.ShoppingCartItem.Quantity, request.ShoppingCartItem.Color, request.ShoppingCartItem.Price, request.ShoppingCartItem.ProductName);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new AddItemIntoBasketResult(shoppingCart.Id);
+    }
+}
