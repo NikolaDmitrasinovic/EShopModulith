cea11eeb7564b63ab819d3d0a2e4bcf4920ab4e1
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-05-11 17:14:39 +0200

    196 remove item from basket

---
+++ src/Modules/Basket/Basket/Basket/Features/RemoveItemFromBasket/RemoveItemFromBasketEndpoint.cs
@@ -0,0 +1,29 @@
+﻿namespace Basket.Basket.Features.RemoveItemFromBasket;
+
+//public record RemoveItemFromBasketRequest(string UserName, Guid ProductId);
+public record RemoveItemFromBasketResponse(Guid Id);
+
+public class RemoveItemFromBasketEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/basket/{userName}/items/{productId}", 
+            async ([FromRoute] string userName,
+            [FromRoute] Guid productId,
+            ISender sender) =>
+        {
+            var command = new RemoveItemFromBasketCommand(userName, productId);
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<RemoveItemFromBasketResponse>();
+
+            return Results.Ok(response);
+        })
+            .WithName("RemoveItemFromBasket")
+            .Produces<RemoveItemFromBasketResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Remove Item from Basket")
+            .WithDescription(("CRemove Item from Basket"));
+    }
+}
