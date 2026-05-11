a3fee0eb3cea69a64fc20367852cba7c89eaf3a1
Nikola Dmitrasinovic <nikola.dmitrasinovic@brighting.nl>
2026-05-11 08:57:57 +0200

    192 add item to basket endpoint

--- src/Modules/Basket/Basket/GlobalUsings.cs
+++ src/Modules/Basket/Basket/GlobalUsings.cs
@@ -12,3 +12,5 @@ global using Carter;
 global using MediatR;
 global using Microsoft.AspNetCore.Http;
 global using Microsoft.AspNetCore.Routing;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Mvc;
